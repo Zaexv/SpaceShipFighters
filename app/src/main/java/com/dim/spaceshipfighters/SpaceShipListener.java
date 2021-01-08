73fa20ae2638b31a1c148cf6bae7224662bfe874
Andres Rodriguez <andresalejan3@gmail.com>
2021-01-08 23:00:47 +0100

    Movimiento independiente de cada nave

--- app/src/main/java/com/dim/spaceshipfighters/SpaceShipListener.java
+++ app/src/main/java/com/dim/spaceshipfighters/SpaceShipListener.java
@@ -1,22 +1,20 @@
 package com.dim.spaceshipfighters;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.view.View.OnTouchListener;
 
-public class SpaceShipGame extends AppCompatActivity {
+public class SpaceShipListener {
 
-    private int numPlayers; //Stores num of players in-game
-    ImageView spaceship1, spaceship2, spaceship3, spaceship4;
     private ViewGroup mainLayout;
-
+    private ImageView image1;
+    private ImageView image2;
 
     private int xDelta;
     private int yDelta;
@@ -56,47 +54,5 @@ public class SpaceShipGame extends AppCompatActivity {
                 return true;
             }
         };
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_space_ship_game);
-        numPlayers = getIntent().getIntExtra("numPlayers", 1);
-
-        spaceship1 = (ImageView) findViewById(R.id.imageP1);
-        spaceship2 = (ImageView) findViewById(R.id.imageP2);
-        spaceship3 = (ImageView) findViewById(R.id.imageP3);
-        spaceship4 = (ImageView) findViewById(R.id.imageP4);
-
-        switch(numPlayers){
-            case 2:
-                spaceship2.setEnabled(false);
-                spaceship2.setVisibility(View.INVISIBLE);
-                spaceship4.setEnabled(false);
-                spaceship4.setVisibility(View.INVISIBLE);
-                break;
-            case 3:
-                spaceship2.setEnabled(false);
-                spaceship2.setVisibility(View.INVISIBLE);
-                break;
-            case 4:
-                break;
-        }
-
-        mainLayout = (RelativeLayout) findViewById(R.id.main);
-
-
-        spaceship1.setOnTouchListener(onTouchListener());
-        spaceship2.setOnTouchListener(onTouchListener());
-        spaceship3.setOnTouchListener(onTouchListener());
-        spaceship4.setOnTouchListener(onTouchListener());
-
-        //INICIALIZAR DISTINTAS NAVES SEGUN NUM JUGADORES
-        /*
-        TextView players = (TextView)findViewById(R.id.textView2);
-        players.setText(Integer.toString(numPlayers));
-         */
-
     }
 }
